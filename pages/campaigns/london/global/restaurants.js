8a0e64497b931e53df73682497f5aa969b6b1fe8
Tony de Faria <tonydefaria@iigethr.com>
2020-04-24 14:58:34 +0100

    added uk & global

--- pages/campaigns/london/global/restaurants.js
+++ pages/campaigns/london/global/restaurants.js
@@ -1,11 +1,11 @@
-// London UK Restaurants
+// London Global Restaurants
 
 // Components
 import Head from "next/head"
-import PostcardOne from "../../../../lib/components/campaigns/london/uk/restaurants/postcard_one"
-import PostcardTwo from "../../../../lib/components/campaigns/london/uk/restaurants/postcard_two"
-import PostcardThree from "../../../../lib/components/campaigns/london/uk/restaurants/postcard_three"
-import GridOne from "../../../../lib/components/campaigns/london/uk/restaurants/grid_one"
+import PostcardOne from "../../../../lib/components/campaigns/london/global/restaurants/postcard_one"
+import PostcardTwo from "../../../../lib/components/campaigns/london/global/restaurants/postcard_two"
+import PostcardThree from "../../../../lib/components/campaigns/london/global/restaurants/postcard_three"
+import GridOne from "../../../../lib/components/campaigns/london/global/restaurants/grid_one"
 // Modules
 import ZahaAlignments from "@iigethr/zaha_alignments"
 
@@ -20,8 +20,8 @@ class CampaignOne extends React.Component {
         <Head>
           <title>Benefit from a premium service of reservations for restaurants | Twice Agency</title>
           <meta name="description" content="We are here to advise you for any sort of reservation, be it short notice or an already fully booked venue." />
-          <meta property="og:url" content="https://weare.twice-agency.com/campaigns/london/uk/restaurants" />
-          <link rel="canonical" href="https://weare.twice-agency.com/campaigns/london/uk/restaurants" />
+          <meta property="og:url" content="https://weare.twice-agency.com/campaigns/london/global/restaurants" />
+          <link rel="canonical" href="https://weare.twice-agency.com/campaigns/london/global/restaurants" />
         </Head>
         <hr className="separator-m" id="top" />
         <PostcardOne />
